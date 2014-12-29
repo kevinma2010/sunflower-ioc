55cbe0a8dd4dc70d4cf5c31defc1989908c1dfd6
Lenbo Ma <mlongbo@gmail.com>
2014-12-29 16:52:04 +0800

    增加注解类

---
+++ src/main/java/com/mlongbo/sunflower/ioc/annotation/Resource.java
@@ -0,0 +1,20 @@
+package com.mlongbo.sunflower.ioc.annotation;
+
+import static java.lang.annotation.ElementType.*;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author malongbo
+ * @date 2014/12/29
+ * @package com.mlongbo.sunflower.ioc.annotation
+ */
+@Target({FIELD, METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Resource {
+    
+    String name() default "";
+    
+}
