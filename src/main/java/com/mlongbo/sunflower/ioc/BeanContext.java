cff11cdb56639c9bd71f6ac86a1160911cb87b33
Lenbo Ma <mlongbo@gmail.com>
2015-01-02 16:13:47 +0800

    实现对象初始化和依赖注入

--- src/main/java/com/mlongbo/sunflower/ioc/BeanContext.java
+++ src/main/java/com/mlongbo/sunflower/ioc/BeanContext.java
@@ -1,7 +1,16 @@
 package com.mlongbo.sunflower.ioc;
 
+import com.mlongbo.sunflower.ioc.annotation.Bean;
+import com.mlongbo.sunflower.ioc.annotation.Resource;
+import com.mlongbo.sunflower.ioc.util.StringUtil;
+import org.apache.commons.beanutils.PropertyUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author malongbo
@@ -9,21 +18,112 @@ import java.util.Map;
  * @package com.mlongbo.sunflower.ioc
  */
 public class BeanContext {
-    private static BeanContext me = new BeanContext();
-    
+    private static BeanContext me = new BeanContext();//单例对象
+    private boolean isInitialized =  false;//是否已初始化
+
+    //存放bean
     private Map<String, Object> beans = new HashMap<String, Object>();
+    //记录依赖关系
     private Map<String, String> dependencies = new HashMap<String, String>();
-    
+
+    /**
+     * 获取实体对象
+     * @return BeanContext Instance
+     */
     public static BeanContext me() {
-        return me;
         
+        return me;
     }
 
-    public Map<String, Object> getBeans() {
-        return beans;
+    /**
+     * 获取bean对象
+     * @param name beanName
+     * @return
+     */
+    public Object getBean(String name) {
+
+        return beans.get(name);
+    }
+    /**
+     * 初始化各对象及依赖
+     * @param classes 扫描到的类集合
+     */
+    public void init(Set<Class<?>> classes) {
+        if (isInitialized || classes == null || classes.size() == 0) {
+            return;
+        }
+
+        //创建bean,扫描依赖关系
+        this.createBeansAndScanDependencies(classes);
+
+        //注入依赖
+        this.injectBeans();
+
+        this.isInitialized = true;
     }
 
-    public Map<String, String> getDependencies() {
-        return dependencies;
+    /**
+     * 扫描注解,创建对象,记录依赖关系
+     * @param classes 类集合
+     */
+    private void createBeansAndScanDependencies(Set<Class<?>> classes) {
+
+        Iterator<Class<?>> iterator = classes.iterator();
+        while (iterator.hasNext()) {
+            Class<?> item = iterator.next();
+            Bean annotation = item.getAnnotation(Bean.class);
+            if (annotation != null) {
+                String beanName = annotation.value();
+                try {
+                    this.beans.put(annotation.value(), item.newInstance());
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+                /*
+                记录依赖关系
+                 */
+                Field[] fields = item.getDeclaredFields();
+
+                for (int i = 0; i < fields.length; i++) {
+                    Field field = fields[i];
+                    Resource fieldAnnotation = field.getAnnotation(Resource.class);
+                    if (fieldAnnotation != null) {
+                        //获取依赖的bean的名称,如果为null,则使用字段名称
+                        String resourceName = fieldAnnotation.value();
+                        if (StringUtil.isEmpty(resourceName)) {
+                            resourceName = field.getName();
+                        }
+
+                        this.dependencies.put(beanName.concat(".").concat(field.getName()), resourceName);
+                    }
+
+                }
+            }
+        }
+    }
+    /**
+     * 扫描依赖关系并注入bean
+     */
+    private void injectBeans() {
+
+        Iterator<Map.Entry<String, String>> iterator = dependencies.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> item = iterator.next();
+            String key = item.getKey();
+            String value = item.getValue();//依赖对象的值
+            String[] split = key.split("\\.");//数组第一个值表示bean对象名称,第二个值为字段属性名称
+            try {
+                PropertyUtils.setProperty(beans.get(split[0]), split[1], beans.get(value));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
